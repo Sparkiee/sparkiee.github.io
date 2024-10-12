6e59154b99e2741bed12a38fbc796a136df38168
Maxwell <maxims970@gmail.com>
2024-10-12 13:35:03 +0300

    Add another project, add some styling, working contact form

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-
-window.addEventListener('scroll', function() {
-    if(window.scrollY != 0) {
-        document.querySelector('#back-to-top').classList.add('scrolled');
-        return;
-    }
-    document.querySelector('#back-to-top').classList.remove('scrolled');
+window.addEventListener("scroll", function () {
+  if (window.scrollY != 0) {
+    document.querySelector("#back-to-top").classList.add("scrolled");
+    return;
+  }
+  document.querySelector("#back-to-top").classList.remove("scrolled");
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('contact-form');
-  
-  form.addEventListener('submit', function(event) {
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contact-form");
+
+  form.addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the default form submission
     var params = {
-        from_name: document.getElementById('name').value,
-        email_id: document.getElementById('email').value,
-        message: document.getElementById('message').value
-    }
-    console.log(params);
-    
-    emailjs.send('service_qzedygh', 'template_bwb60hj', params).then(function(response) {
-        console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-        console.log('FAILED...', error);
-    });
-    console.log('Form submitted!');
+      from_name: document.getElementById("name").value,
+      email_id: document.getElementById("email").value,
+      message: document.getElementById("message").value
+    };
+
+    emailjs.send("service_qzedygh", "template_bwb60hj", params).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
+    console.log("Form submitted!");
   });
+
 });
